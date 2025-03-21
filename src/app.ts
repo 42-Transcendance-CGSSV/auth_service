e0b8159db99b0d7724f9e8b3693cd073158eba37
vSKAH <vskahhh@gmail.com>
2025-03-21 17:08:26 +0100

    adding support of .env file

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import fastify from "fastify";
+import dotenv from "dotenv";
 
 const app = fastify({ logger: true });
+dotenv.config();
 
+//TODO: FIXING ENV UNDEFINED
 async function start(): Promise<void> {
     try {
-        await app.listen({ port: 8080 });
+        await app.listen({ port: Number(process.env.PORT) });
     } catch (error) {
         app.log.error(error);
         process.exit(1);
     }
 }
 
-app.get("/healthcheck", (_req, response) => {
+app.get(process.env.BASE_ROUTE + "/healthcheck", (_req, response) => {
     response.send({ message: "Success" });
 });
 
