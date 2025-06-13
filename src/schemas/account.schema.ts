cbb8318763ac6b79e4697d0dc80190daac4c937a
vSKAH <vskahhh@gmail.com>
2025-06-13 16:59:03 +0200

    update: schemas

--- src/schemas/account.schema.ts
+++ src/schemas/account.schema.ts
@@ -3,13 +3,14 @@ import schema from "fluent-json-schema";
 export const getAccountSchema = schema
     .object()
     .anyOf([
-        schema.object().prop("name", schema.string().minLength(4).maxLength(16).pattern("^[A-Za-z]+$")).required(),
-        schema.object().prop("user_id", schema.number()).required()
+        schema.object().prop("name", schema.string().minLength(4).maxLength(16).pattern("^[a-zA-Z0-9_]$")).required(),
+        schema.object().prop("user_id", schema.number().minimum(1)).required()
     ]);
 
+//TODO: regex external token
 export const updateAccountSchema = schema
     .object()
-    .prop("name", schema.string().minLength(4).maxLength(16).pattern("^[A-Za-z]+$"))
+    .prop("name", schema.string().minLength(4).maxLength(16).pattern("^[a-zA-Z0-9_]$"))
     .prop("email", schema.string().format("email").minLength(3).maxLength(320))
     .prop("password", schema.string().minLength(8).maxLength(32))
     .prop("external_token", schema.string());
--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -2,7 +2,7 @@ import schema from "fluent-json-schema";
 
 export const registerSchema = schema
     .object()
-    .prop("name", schema.string().minLength(4).maxLength(16).pattern("^[A-Za-z]+$").required())
+    .prop("name", schema.string().minLength(4).maxLength(16).pattern("^[a-zA-Z0-9_]$").required())
     .prop("email", schema.string().format("email").minLength(3).maxLength(320).required())
     .prop("password", schema.string().minLength(8).maxLength(32).required());
 
@@ -14,5 +14,5 @@ export const loginSchema = schema
 export const totpVerifySchema = schema
     .object()
     .prop("code", schema.string().format("regex").pattern(/^\d+$/).minLength(6).maxLength(6).required())
-    .prop("user_id", schema.number())
+    .prop("user_id", schema.number().minimum(1))
     .required();
