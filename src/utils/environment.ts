1fc26093f1cb457818b487842c73b7ada13d0c13
vSKAH <vskahhh@gmail.com>
2025-06-13 15:25:02 +0200

    update launcher

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -3,6 +3,5 @@ export const env = {
     LOG_LEVEL: process.env.LOG_LEVEL || "debug",
     BREVO_API_KEY: process.env.BREVO_API_KEY || "default_api_key",
     LOG_TIME_FORMAT: process.env.LOG_TIME_FORMAT || "${day}:${month}:${year} ${hours}:${minutes} (${seconds}:${milliseconds})",
-    DB_TOTP_TABLE: process.env.DB_TOTP_TABLE || "two_factor",
     JWT_SECRET: process.env.JWT_SECRET || "default_secret"
 };
